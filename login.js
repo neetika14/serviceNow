8fa0e7f434501802bfd4901eaed1223fcfca1e86
Divyanshikhatri062 <109607345+Divyanshikhatri062@users.noreply.github.com>
2023-01-28 09:23:44 +0530

    Add files via upload

--- login.js
+++ login.js
@@ -1,14 +1,23 @@
-const get = elem => document.getElementById(elem),
-					 registerButton = get('register'),
-					 loginButton = get('login'),
-					 container = get('container')
- 
-registerButton.onclick = () => {
-					 container.className = "active"
+const signUpButton = document.getElementById("signUp");
+const signInButton = document.getElementById("signIn");
+const container = document.getElementById("container");
+
+signUpButton.addEventListener("click", () => {
+    container.classList.add("right-panel-active");
+});
+
+signInButton.addEventListener("click", () => {
+    container.classList.remove("right-panel-active");
+});
+
+function myFunction___ad() {
+    var signIn = document.getElementById("container");
+    signIn.classList.toggle("sign-in-container__show");
+    signIn.classList.remove("sign-up-container__show");
 }
 
-loginButton.onclick = () => {
-						container.className = "close"
+function myFunction___hi() {
+    var signUp = document.getElementById("container");
+    signUp.classList.toggle("sign-up-container__show");
+    signUp.classList.remove("sign-in-container__show");
 }
- 
-
